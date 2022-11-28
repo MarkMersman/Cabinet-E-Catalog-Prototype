7ef4537693932ee5cf86e738aac641a87f2c4ff5
Mark Mersman <markmersman@gmail.com>
2022-11-28 13:12:25 -0600

    connected front and backend with api call and response

--- public/main.js
+++ public/main.js
@@ -1,9 +1,24 @@
+let drawerID = '637c0e10fd446cd372bbff3a'
+let frameID = '';
+let doorID = '';
+let styleID = '';
+let woodID = '';
+
+//document.getElementById("SubmitButton").addEventListener("click", function(){alert("Hello world!");});
+
 clickAndSelect()
+
 
 function clickAndSelect() {
   let cards = Array.from( document.querySelectorAll('.card') ),
       elements = []
   
+  let frames = Array.from(document.querySelectorAll('.frameCard'));    
+  let woods = Array.from(document.querySelectorAll('.woodCard'));    
+  let doors = Array.from(document.querySelectorAll('.doorCard'));    
+  let styles = Array.from(document.querySelectorAll('.styleCard'));      
+  
+
   // Add child nodes to clickable elements
   cards.forEach(card => {
     elements = elements.concat( Array.from(card.children) )
@@ -36,17 +51,75 @@ function clickAndSelect() {
     
         if(card.classList.contains('visited')){
             card.classList.remove('visited');
-        }else{
-            card.classList.add('visited');  
-            console.log(card.id);
+        }else if(card.classList.contains('frameCard')){
+          for (var c in frames){
+            if(frames[c].classList.contains('visited')){
+              frames[c].classList.remove('visited')
+            }            
+          }
+          card.classList.add('visited');
+          frameID = card.getAttribute('data-value');
+        }else if(card.classList.contains('woodCard')){
+          for (var c in woods){
+            if(woods[c].classList.contains('visited')){
+              woods[c].classList.remove('visited')
+            }            
+          }
+          card.classList.add('visited');
+          woodID = card.getAttribute('data-value');
+        }else if(card.classList.contains('doorCard')){
+          for (var c in doors){
+            if(doors[c].classList.contains('visited')){
+              doors[c].classList.remove('visited')
+            }            
+          }
+          card.classList.add('visited');
+          doorID = card.getAttribute('data-value');
+        }else if(card.classList.contains('styleCard')){
+          for (var c in styles){
+            if(styles[c].classList.contains('visited')){
+              styles[c].classList.remove('visited')
+            }            
+          }
+          card.classList.add('visited');
+          styleID = card.getAttribute('data-value');
         }
-        
-        
-      }
-  
+        else{            
+            card.classList.add('visited');              
+        }  
+      }  
       // Clean up
-      card.removeAttribute('data-md')
-      
+      card.removeAttribute('data-md')      
     })    
   })  
 }
+
+async function submit(){  
+
+  
+  //window.location.href = "./price.html";
+
+  try {
+    await axios.post('api/v1/price-info',
+    {
+      frameType: `${frameID}`,
+      woodType: `${woodID}`,
+      doorType: `${doorID}`,
+      doorStyle: `${styleID}`,
+      drawerStyle: `${drawerID}`,
+      woGrain: ``,
+      numDrawers: 1 
+    }).then(function (response) {     
+      console.log(response.data);
+      var res = JSON.stringify(response.data, null, 4);
+      //var newElement = `<div>${res}</div>`;
+      document.getElementById("json").innerHTML = res; 
+    }).catch(function (error){
+      console.log(error);
+    })   
+  }catch (error){
+    console.log(error);
+  } 
+  
+}
+
